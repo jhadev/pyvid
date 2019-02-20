9ad2aec6050e91ba23a9c0ec79ebaa07a9116fc4
JA <joshappeldev@gmail.com>
2019-02-20 17:39:39 -0500

    bootstrap table

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 # Create your views here.
 
 # main page
 def index(request):
-    return HttpResponse('Hello World')
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
