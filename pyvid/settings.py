239c466413dfb9349bc0007c5a1e3db109e4ef69
JA <joshappeldev@gmail.com>
2019-02-21 14:03:26 -0500

    api creation

--- pyvid/settings.py
+++ pyvid/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies.apps.MoviesConfig'
+    'tastypie',
+    'movies.apps.MoviesConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
